f56d6127ca9d975915c3d801a8592b398ccad573
Frederic Jacob <frederic.jacob.78@gmail.com>
2017-10-31 15:01:49 -0400

    Initial commit

---
+++ spec/atom-checkpatch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCheckpatchView from '../lib/atom-checkpatch-view';
+
+describe('AtomCheckpatchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
