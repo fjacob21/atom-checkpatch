4631c80e592a7901168012836205bbbdb5f9077f
Frederic Jacob <frederic.jacob.78@gmail.com>
2017-11-08 10:52:03 -0500

    Change the default Linux kernel src path

--- spec/atom-checkpatch-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomCheckpatchView from '../lib/atom-checkpatch-view';
-
-describe('AtomCheckpatchView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
